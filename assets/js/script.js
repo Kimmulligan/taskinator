bb36bb4737609b8280bd5b50d39390127b27014f
Kim Mulligan <kimmariemulligan@gmail.com>
2020-08-30 19:06:42 -0700

    added task input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(e){
+  e.preventDefault();
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
+  console.log()
   tasksToDoEl.appendChild(listItemEl);
 }
 
